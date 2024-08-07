07ac0c8dfb28c72fd35355a7c00054f0e434a235
MattiaP7 <7mattiapirazzi@gmail.com>
2024-08-07 23:04:10 +0200

    First commit

---
+++ lua/vim-opts.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+vim.opt.mouse = 'a'
+
+vim.g.mapleader = " "
