89310fe6a60ecc05a0f05aa92423624f758fc316
MattiaP7 <7mattiapirazzi@gmail.com>
2024-08-08 19:29:05 +0200

    idk

---
+++ lua/plugins/coc.lua
@@ -0,0 +1,10 @@
+return  {
+	{
+		'neoclide/coc.nvim', 
+		branch = 'release'
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	}	
+}
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,17 +4,9 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-		--formatting
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-		
-		--diagnostic
-		null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.clang_format.with({
-            filetypes = { "c", "cpp", "objc", "objcpp" },
-            args = { "-style=file" }, -- Pass additional arguments if needed
-        }),
-	},
+        null_ls.builtins.formatting.prettier
+      },
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
